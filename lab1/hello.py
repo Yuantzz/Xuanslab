ba9c3e41e23bbe156e5321f0b9fc989e1dc02a2f
Yuantzz <fxuuuu@163.com>
2023-09-19 21:01:52 +0800

    Q3

--- lab1/hello.py
+++ lab1/hello.py
@@ -1,1 +1,10 @@
-print("傅渲 10225501418")
+for i in range(0, 7):
+    print(chr(0x2605), end='')
+
+print(chr(0x2605))
+print(chr(0x2605), end='')
+print("数据工程导论", end='')
+print(chr(0x2605))
+
+for i in range(0, 8):
+    print(chr(0x2605), end='')
